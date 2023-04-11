0b713a9fdff3a7e418bbe58a12f0ffc7ec6e2088
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2023-04-12 06:31:08 +0900

    fixed folder name space bug

--- scripts/layer_divider_modules/ui_utils.py
+++ scripts/layer_divider_modules/ui_utils.py
@@ -3,6 +3,6 @@ import os
 
 def open_folder(folder_path):
     if os.path.exists(folder_path):
-        os.system(f"start {folder_path}")
+        os.system(f'start "" "{folder_path}"')
     else:
-        print(f"The folder {folder_path} does not exist.")
+        print(f"The folder '{folder_path}' does not exist.")
