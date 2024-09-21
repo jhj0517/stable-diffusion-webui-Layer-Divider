bd953d3db9bb5617f202f5571a53cf3e4d379e71
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-09-21 15:50:42 +0900

    Add paths

--- scripts/layer_divider_modules/paths.py
+++ scripts/layer_divider_modules/paths.py
@@ -8,3 +8,16 @@ SAM2_MODEL_CONFIGS = {
     "sam2_hiera_base_plus": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_b+.yaml"),
     "sam2_hiera_large": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_l.yaml"),
 }
+OUTPUT_DIR = os.path.join(LAYER_DIVIDER_EXTENSION_DIR, "outputs")
+OUTPUT_PSD_DIR = os.path.join(OUTPUT_DIR, "psd")
+OUTPUT_FILTER_DIR = os.path.join(OUTPUT_DIR, "filter")
+TEMP_DIR = os.path.join(LAYER_DIVIDER_EXTENSION_DIR, "temp")
+TEMP_OUT_DIR = os.path.join(TEMP_DIR, "out")
+
+for dir_path in [SAM2_CONFIGS_DIR,
+                 OUTPUT_DIR,
+                 OUTPUT_PSD_DIR,
+                 OUTPUT_FILTER_DIR,
+                 TEMP_DIR,
+                 TEMP_OUT_DIR]:
+    os.makedirs(dir_path, exist_ok=True)
