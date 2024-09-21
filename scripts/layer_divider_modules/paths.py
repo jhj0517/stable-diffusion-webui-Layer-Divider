36c624adf301dbeac71cf4dc577426b54daa1a55
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-09-21 15:55:55 +0900

    Add models dir

--- scripts/layer_divider_modules/paths.py
+++ scripts/layer_divider_modules/paths.py
@@ -1,7 +1,10 @@
 import os
 
+from modules.paths import models_path
+
 LAYER_DIVIDER_EXTENSION_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 SAM2_CONFIGS_DIR = os.path.join(LAYER_DIVIDER_EXTENSION_DIR, "configs")
+SAM2_MODEL_DIR = os.path.join(models_path, "sam2")
 SAM2_MODEL_CONFIGS = {
     "sam2_hiera_tiny": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_t.yaml"),
     "sam2_hiera_small": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_s.yaml"),
@@ -14,7 +17,8 @@ OUTPUT_FILTER_DIR = os.path.join(OUTPUT_DIR, "filter")
 TEMP_DIR = os.path.join(LAYER_DIVIDER_EXTENSION_DIR, "temp")
 TEMP_OUT_DIR = os.path.join(TEMP_DIR, "out")
 
-for dir_path in [SAM2_CONFIGS_DIR,
+for dir_path in [SAM2_MODEL_DIR,
+                 SAM2_CONFIGS_DIR,
                  OUTPUT_DIR,
                  OUTPUT_PSD_DIR,
                  OUTPUT_FILTER_DIR,
